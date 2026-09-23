224c7a2986519d0f218d98c913587d5c8e669eb7
flitternie <flitternie@gmail.com>
2026-09-23 13:46:08 -0500

    Add Adobe confidential headers

--- utils/report_generator_batch.py
+++ utils/report_generator_batch.py
@@ -1,3 +1,17 @@
+# ADOBE CONFIDENTIAL
+#
+# Copyright 2026 Adobe
+# All Rights Reserved.
+#
+# NOTICE: All information contained herein is, and remains
+# the property of Adobe and its suppliers, if any. The intellectual
+# and technical concepts contained herein are proprietary to Adobe
+# and its suppliers and are protected by all applicable intellectual
+# property laws, including trade secret and copyright laws.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Adobe.
+
 import os
 import json
 import asyncio
@@ -265,4 +279,3 @@ async def main_async():
 if __name__ == "__main__":
     asyncio.run(main_async())
 
-    
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,17 @@
+# ADOBE CONFIDENTIAL
+#
+# Copyright 2026 Adobe
+# All Rights Reserved.
+#
+# NOTICE: All information contained herein is, and remains
+# the property of Adobe and its suppliers, if any. The intellectual
+# and technical concepts contained herein are proprietary to Adobe
+# and its suppliers and are protected by all applicable intellectual
+# property laws, including trade secret and copyright laws.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Adobe.
+
 import json
 from typing import Dict, List, Any, Optional
 from datetime import datetime
@@ -285,4 +299,3 @@ class ResearchLogger:
         
         if self.visualization:
             self.visualizer.visualize(output_file=f"{self.logs_dir}/research_visualization.html")
-
